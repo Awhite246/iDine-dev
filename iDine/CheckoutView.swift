3fdc20b95c3218fd8135de9d118157ae3616953f
Alistiar White <awhite@bsd220.org>
2022-09-02 09:45:42 -0500

    created checkoutView

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,34 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Alistair White on 9/2/22.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    @State private var paymentType = "Cash"
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView().environmentObject(Order())
+    }
+}
+
