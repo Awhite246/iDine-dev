119a7349a76d43390caf85da2c0d6b88504cc828
Alistiar White <awhite@bsd220.org>
2022-09-02 09:43:10 -0500

    Added tabview

---
+++ iDine/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  MainView.swift
+//  iDine
+//
+//  Created by Alistair White on 9/2/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+
+            OrderView()
+                .tabItem {
+                    Label("Order", systemImage: "square.and.pencil")
+                }
+        }
+    }
+}
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView().environmentObject(Order())
+    }
+}
